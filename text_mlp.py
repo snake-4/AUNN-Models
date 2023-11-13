c501c92a32d7344a216ae4e08f507a8ff5fec9cd
snake-4 <18491360+snake-4@users.noreply.github.com>
2023-11-13 04:30:22 +0100

    Added meta-learning code...
    and changed parameters of text_mlp.

--- text_mlp.py
+++ text_mlp.py
@@ -4,7 +4,6 @@ import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from random import randint
 
 from impl.utils import *
 from impl.mlp_models import *
@@ -15,29 +14,28 @@ device = get_torch_device()
 train_model = True  # Set to False to only evaluate the model
 model_path = "./data/text_model.pt"
 
-training_epochs = 2
-training_batch_size = 1000
-
 # Input is a 64-bit index. Output is a probability vector over possible byte values
 model_input_width = 64
-model = InvertedBottleneckMLP(
+model = NormalMLP(
     in_dim=model_input_width,
     out_dim=256,
-    hidden_width=256,
-    hidden_depth=32,
+    hidden_width=1024,
+    hidden_depth=512,
+    is_residual=True
 ).to(device)
 
 # Trial and error <3. Feel free to change these parameters.
-optimizer = optim.AdamW(model.parameters(), lr=1e-3)
+training_epochs = 2
+training_batch_size = 1000
+optimizer = optim.SGD(model.parameters(), lr=1e-3)
 scheduler = StepLR(optimizer, step_size=5000, gamma=0.9)
 lossFunc = nn.CrossEntropyLoss()
 
 textDataset = TextModelDataset()
 textLoader = DataLoader(textDataset, shuffle=False, batch_size=training_batch_size)
 
-def train():
-    highestTrainedIndex = 0
 
+def train():
     model.train()
     for _ in tqdm(range(training_epochs)):
         for batch_idx, batchValues in enumerate(tqdm(textLoader, leave=False)):
@@ -47,32 +45,42 @@ def train():
 
             beginIndex = batch_idx * len(batchValues)
             endIndex = beginIndex + len(batchValues) - 1
-            highestTrainedIndex = max(highestTrainedIndex, endIndex)
 
             inputs = binary_arange(beginIndex, endIndex + 1, model_input_width, device)
             output = model(inputs).reshape(-1, 256)
-            
+
             loss = lossFunc(output, batchValues)
             loss.backward()
             optimizer.step()
             scheduler.step()
 
     torch.save(model.state_dict(), model_path)
-    print(f"Training finished. Model saved. Highest trained index: {highestTrainedIndex}")
+    print(f"Training finished. Model saved.")
+
+
+def evalAndMetaTrain(optimizerArg, modelInput):
+    optimizerArg.zero_grad()
+    output = torch.softmax(model(modelInput), dim=0)
+    sampledTensor = torch.multinomial(output, num_samples=1)
+    # CrossEntropyLoss expects batched inputs
+    loss = lossFunc(output.reshape(-1, 256), sampledTensor)
+    loss.backward()
+    optimizerArg.step()
+    return sampledTensor
 
 
 def evaluate():
-    model.eval()
-    # TODO: change this?
-    evalOffset = randint(0, len(textDataset)-5000)
-    evalTokenCount = 5000
-    
-    inputs = binary_arange(evalOffset, evalOffset +  evalTokenCount, model_input_width, device)
-    output = torch.softmax(model(inputs), dim=1)
-    samples = torch.multinomial(output, num_samples=1)
+    model.train()
+    evalOptimizer = optim.SGD(model.parameters(), lr=1e-4)
+    evalOffset = len(textDataset)
+    evalTokenCount = 100
 
-    for token in samples:
-        print(chr(token.item()), end='')
+    for idx in range(evalOffset, evalOffset + evalTokenCount):
+        encodedIdx = binary_encode_tensor(
+            torch.tensor(idx).to(device), model_input_width
+        )
+        sampledTensor = evalAndMetaTrain(evalOptimizer, encodedIdx)
+        print(chr(sampledTensor.item()), end="")
 
 
 if train_model:
